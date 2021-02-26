1cad23862adee36aa6fe0c36375b837afd2a99ed
ilromali <iliashenkoroman@gmail.com>
2021-02-26 10:46:51 -0800

    add basic file tree for frontend

---
+++ Nutri/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store = {store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
