90672b80a34788cfd539c2e321088ebbc6ee9e21
ilromali <iliashenkoroman@gmail.com>
2021-02-26 15:00:55 -0800

    add initial migration

---
+++ NutriAPI/Migrations/20210226225343_Initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NutriAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Supplements",
+                columns: table => new
+                {
+                    SupplementId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Type = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Dosing = table.Column<string>(nullable: false),
+                    Contraindications = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Supplements", x => x.SupplementId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Supplements",
+                columns: new[] { "SupplementId", "Contraindications", "Dosing", "Name", "Type" },
+                values: new object[] { 1, "Milk Thistle may lower blood sugar levels in people with type 2 diabetes. Those with allergies to plants in the same family as Milk Thistle (ragweed, marigold, etc.) may experience a reaction.", "According to research, the therapeutic threshold may be around 225mg per day.", "Milk Thistle", "Herb" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Supplements");
+        }
+    }
+}
