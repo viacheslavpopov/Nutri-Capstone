18eaa96c6f1e1d86eafde8386b0a890fcb04fc63
ilromali <iliashenkoroman@gmail.com>
2021-02-26 14:58:33 -0800

    add missing semicolon, fix incorrect directive

--- NutriAPI/Controllers/SupplementsController.cs
+++ NutriAPI/Controllers/SupplementsController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Nutree.Models;
+using NutriAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- NutriAPI/Models/NutriAPIContext.cs
+++ NutriAPI/Models/NutriAPIContext.cs
@@ -14,7 +14,7 @@ namespace NutriAPI.Models
       builder.Entity<Supplement>()
         .HasData(
           new Supplement { SupplementId = 1, Type = "Herb", Name = "Milk Thistle", Dosing = "According to research, the therapeutic threshold may be around 225mg per day.", Contraindications = "Milk Thistle may lower blood sugar levels in people with type 2 diabetes. Those with allergies to plants in the same family as Milk Thistle (ragweed, marigold, etc.) may experience a reaction."}
-        )
+        );
     }
   }
 }
