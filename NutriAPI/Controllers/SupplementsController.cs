d0f981406c850a4a66505f03475d1a1805264df1
ilromali <iliashenkoroman@gmail.com>
2021-02-26 16:30:43 -0800

    add queryable GET route

--- NutriAPI/Controllers/SupplementsController.cs
+++ NutriAPI/Controllers/SupplementsController.cs
@@ -20,5 +20,28 @@ namespace NutriAPI.Controllers
     {
       _db = db;
     }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Supplement>> Get(string type, string name, string dosing, string contraindications)
+    {
+      var query = _db.Supplements.AsQueryable();
+      if (type != null)
+      {
+        query = query.Where(entry => entry.Type == type);
+      }
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+      if (dosing != null)
+      {
+        query = query.Where(entry => entry.Dosing == dosing);
+      }
+      if (contraindications != null)
+      {
+        query = query.Where(entry => entry.Contraindications == contraindications);
+      }
+      return query.ToList();
+    }
   }
 }
