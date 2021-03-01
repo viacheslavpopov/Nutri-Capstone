d965307462cff2f8fbb0e0b273c26ceb6c7c6c56
Chelsea Becker <chelraebecker@gmail.com>
2021-02-28 17:07:05 -0800

    add App structure

---
+++ Nutri/src/components/App.js
@@ -0,0 +1,15 @@
+import './../App.css';
+import React from 'react';
+import NavBar from './NavBar';
+import SupplementControl from './SupplementControl';
+
+function App() {
+    return (
+        <div className="App">
+            <NavBar />
+            <SupplementControl />
+        </div>
+    );
+}
+
+export default App;
