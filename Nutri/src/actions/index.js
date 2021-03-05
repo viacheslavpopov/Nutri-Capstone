1bc214d04a3d8864d4be97897241222a74bec53c
Chelsea Becker <chelraebecker@gmail.com>
2021-03-05 09:17:17 -0800

    add toggle details and supp list

--- Nutri/src/actions/index.js
+++ Nutri/src/actions/index.js
@@ -3,3 +3,11 @@ import * as c from './ActionTypes';
 export const toggleMal = () => ({
     type: c.TOGGLE_MAL
 });
+
+export const toggleDetails = () => ({
+    type: c.TOGGLE_DETAILS
+});
+
+export const toggleSupList = () => ({
+    type: c.TOGGLE_SUP_LIST
+});
