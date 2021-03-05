2efd4144d770fdc4b86913df7816fd26dda45f17
Chelsea Becker <chelraebecker@gmail.com>
2021-03-05 09:56:47 -0800

    add combineReducers from redux library

--- Nutri/src/reducers/index.js
+++ Nutri/src/reducers/index.js
@@ -1,6 +1,7 @@
 import suppDetailsVisibleReducer from './supp-details-visible-reducer';
 import suppListVisibleReducer from './supp-list-visible-reducer';
 import malListVisibleReducer from './mal-list-visible-reducer';
+import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
     suppDetailsVisibleOnPage: suppDetailsVisibleReducer,
