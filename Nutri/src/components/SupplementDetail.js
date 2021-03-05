e3e6fbe40243f8ab47aecd07523d72592fb4b017
Chelsea Becker <chelraebecker@gmail.com>
2021-03-05 08:58:49 -0800

    add boilerplate to supplement detail

---
+++ Nutri/src/components/SupplementDetail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function SupplementDetail(props) {
+    // deconstruct contants from props
+    // return (
+    //   return display structure  
+    // );
+}
+
+SupplementDetail.propTypes = {
+    // pass in supplement as a prop
+    // no functions needed, read only (no delete or edit functionality)
+}
+
+export default SupplementDetail;
