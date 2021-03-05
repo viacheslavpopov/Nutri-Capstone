696b1ff334f088b0d06187cf634c74fc0f6dbaf6
ilromali <iliashenkoroman@gmail.com>
2021-03-05 12:04:13 -0800

    add another updated migrations file

--- NutriAPI/Migrations/NutriAPIContextModelSnapshot.cs
+++ NutriAPI/Migrations/NutriAPIContextModelSnapshot.cs
@@ -27,6 +27,9 @@ namespace NutriAPI.Migrations
                     b.Property<string>("Dosing")
                         .IsRequired();
 
+                    b.Property<string>("Maladies")
+                        .IsRequired();
+
                     b.Property<string>("Name")
                         .IsRequired();
 
@@ -43,6 +46,7 @@ namespace NutriAPI.Migrations
                             SupplementId = 1,
                             Contraindications = "Milk Thistle may lower blood sugar levels in people with type 2 diabetes. Those with allergies to plants in the same family as Milk Thistle (ragweed, marigold, etc.) may experience a reaction.",
                             Dosing = "According to research, the therapeutic threshold may be around 225mg per day.",
+                            Maladies = "Liver Health, Skin Health, Blood Sugar Support, Detox Support",
                             Name = "Milk Thistle",
                             Type = "Herb"
                         },
@@ -51,6 +55,7 @@ namespace NutriAPI.Migrations
                             SupplementId = 2,
                             Contraindications = "Preformed Vitamin A (as opposed to provitamin A as beta-carotene, which is water-soluble) is fat-soluble, which means that it can accumulate in the body and cause potential toxicity. If you are concerned about this, consult a physician about what dose is right for you.",
                             Dosing = "Consult this webpage to find your appropriate dosing: https://ods.od.nih.gov/factsheets/VitaminA-HealthProfessional/. Keep in mind that Vitamin A is stored in the body long-term, and therefore accumulates. For optimal health, ",
+                            Maladies = "Skin Health, Immune System Support, Eye Health, Antioxidant Support",
                             Name = "Vitamin A",
                             Type = "Vitamin"
                         },
@@ -59,14 +64,16 @@ namespace NutriAPI.Migrations
                             SupplementId = 3,
                             Contraindications = "High doses taken long-term (over a 20-month period) may lead to: hypoglycemia, weakness, shakiness, excessive sweating, or muscular dystrophy. This should also not be taken alongside blood-sugar-lowering medications unless prescribed by your doctor.",
                             Dosing = "According to Dr. Axe, a general recommended gymnema sylvestre dosage is a 100-milligram capsule, taken up to four times daily. It’s best to start with one capsule and gradually increase your dose, paying attention to any adverse effects.",
+                            Maladies = "Blood Sugar Support",
                             Name = "Gymnema Sylvestre",
                             Type = "Herb"
                         },
                         new
                         {
                             SupplementId = 4,
-                            Contraindications = "Research suggests that toxicity does not occur until you have reached above 80 ng/mL AND blood calcium levels increase. Vitamin D can be taken with any other vitamin or herb. ",
+                            Contraindications = "Research suggests that toxicity does not occur until you have reached above 80 ng/mL AND blood calcium levels increase. Vitamin D can be taken with any other vitamin or herb.",
                             Dosing = "Dosing can range from 500IU to upwards of 25,000IU. This will depend on your current Vitamin D status. A doctor can order a blood test to find this out. Optimal Vitamin D levels in the body are 40-65 ng/mL.",
+                            Maladies = "Bone Health, Immune System Support, Hormone Balance",
                             Name = "Vitamin D",
                             Type = "Vitamin"
                         });
