7c33cb83c1552ea96bbc8166fc90b90b91d3f61d
ilromali <iliashenkoroman@gmail.com>
2021-03-05 14:28:56 -0800

    add creators for API requests and API request function

--- Nutri/src/actions/index.js
+++ Nutri/src/actions/index.js
@@ -11,3 +11,32 @@ export const toggleDetails = () => ({
 export const toggleSupList = () => ({
     type: c.TOGGLE_SUP_LIST
 });
+
+export const requestSupplements = () => ({
+    type: c.REQUEST_SUPP
+});
+
+export const getSuppSuccess = (suppData) => ({
+    type: c.GET_SUPP_SUCCESS,
+    suppData
+});
+
+export const getSuppFailure = (error) => ({
+    type: c.GET_SUPP_FAILURE,
+    error
+});
+
+export const makeApiCall = () => {
+    return dispatch => {
+        dispatch(requestSupplements);
+        return fetch(`http://localhost:5000/api/supplements/`) // ${malady} to search by malady clicked by user
+            .then(response => response.json())
+            .then(
+                (jsonifiedResponse) => {
+                    dispatch(getSuppSuccess)(jsonifiedResponse);
+                })
+            .catch((error) => {
+                dispatch(getSuppFailure(error));
+            });
+    }
+}
