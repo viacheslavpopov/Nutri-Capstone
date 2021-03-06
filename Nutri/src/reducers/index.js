0c385c14dd4fb1cc6368a280c3b6b5afef594c29
ilromali <iliashenkoroman@gmail.com>
2021-03-05 16:29:17 -0800

    add databaseReducer to rootReducer

--- Nutri/src/reducers/index.js
+++ Nutri/src/reducers/index.js
@@ -1,12 +1,14 @@
 import suppDetailsVisibleReducer from './supp-details-visible-reducer';
 import suppListVisibleReducer from './supp-list-visible-reducer';
 import malListVisibleReducer from './mal-list-visible-reducer';
+import databaseReducer from './database-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
     suppDetailsVisibleOnPage: suppDetailsVisibleReducer,
     suppListVisibleOnPage: suppListVisibleReducer,
-    malListVisibleOnPage: malListVisibleReducer
+    malListVisibleOnPage: malListVisibleReducer,
+    suppList: databaseReducer
 });
 
 export default rootReducer;
