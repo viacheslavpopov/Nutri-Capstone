cee3a59fea28c3cc95889d96e07c4d44dabeef77
ilromali <iliashenkoroman@gmail.com>
2021-03-05 16:30:31 -0800

    change default state to false

--- Nutri/src/reducers/supp-list-visible-reducer.js
+++ Nutri/src/reducers/supp-list-visible-reducer.js
@@ -1,6 +1,6 @@
 import * as c from './../actions/ActionTypes';
 
-export default (state = true, action) => {
+export default (state = false, action) => {
     switch (action.type) {
         case c.TOGGLE_SUP_LIST:
             return !state;
