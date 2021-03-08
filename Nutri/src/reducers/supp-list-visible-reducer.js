69456e20ed6a7253e5a9640844fc1bf58d0dbc2b
ilromali <iliashenkoroman@gmail.com>
2021-03-08 11:48:10 -0800

    correct action constant

--- Nutri/src/reducers/supp-list-visible-reducer.js
+++ Nutri/src/reducers/supp-list-visible-reducer.js
@@ -2,7 +2,7 @@ import * as c from './../actions/ActionTypes';
 
 export default (state = false, action) => {
     switch (action.type) {
-        case c.TOGGLE_SUP_LIST:
+        case c.TOGGLE_SUPP_LIST:
             return !state;
         default:
             return state;
