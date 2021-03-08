837453aa92b535f924325f155b8312c3eef3c7fe
ilromali <iliashenkoroman@gmail.com>
2021-03-08 11:48:40 -0800

    add buttons with method attached to modify API call

--- Nutri/src/components/MaladyList.js
+++ Nutri/src/components/MaladyList.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function MaladyList() {
-  
+function MaladyList(props) {
+  const { onSelectMalady } = props;
+  return (
+    <>
+      <h2><button onClick={ () => onSelectMalady('sugar')}>Blood Sugar Support</button></h2>
+      <h2><button onClick={ () => onSelectMalady('detox')}>Detox Support</button></h2>
+      <h2><button onClick={ () => onSelectMalady('immune')}>Immune System Support</button></h2>
+      <h2><button onClick={ () => onSelectMalady('liver')}>Liver Health</button></h2>
+      <h2><button onClick={ () => onSelectMalady('skin')}>Skin Health</button></h2>
+    </>
+  )
+}
+
+MaladyList.propTypes = {
+  onSelectMalady: PropTypes.func
 }
 
 export default MaladyList;
+
+// click a malady.. an API call is made with that malady as a parameter?
+// could put the API call function in the malady list component
+
+//onSelectMalady toggles what is visible on page, and is triggered if a malady is clicked
+// after a malady is clicked, an API call needs to be made with that malady as the argument
+// should these both be in one function? 
+// error could be displayed on main page as before if an API call fails using error prop and isLoading
+// can I have a function doing 3 things from here? maybe.. dispatching 2+ actions with API call.
