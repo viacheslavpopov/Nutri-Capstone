88d4349d47ba6657881d8a4061c4f5c4d9230de7
ilromali <iliashenkoroman@gmail.com>
2021-03-09 12:04:24 -0800

    add bootstrap styling

--- Nutri/src/components/Help.js
+++ Nutri/src/components/Help.js
@@ -3,14 +3,22 @@ import React from 'react';
 function Help() {
     return (
         <>
-            <h1>Help Page</h1>
-            <h3>FAQs</h3>
-            <h4>What does 'contraindications' mean?</h4>
-            <p>According to Wikipedia, a contraindication is a condition or factor that serves as a reason to withhold a certain medical treatment due to the harm that it would cause the patient. This means that if you have a certain medical condition that would conflict with the use of a supplement, you should either avoid taking that supplement, or consult with your physician first before taking it.</p>
-            <h4>What is 'malady'?</h4>
-            <p>A malady is defined as a disease or ailment.</p>
-            <h4>My doctor doesn't know much about supplements. How do I find out which is right for me?</h4>
-            <p>It is recommended to find a doctor who <em>is</em> educated on the use of supplements. These doctors are typically Chiropractors and Naturopathic Doctors. You may also want to consult an experienced herbalist or holistic nutritionist. It is best to avoid Googling because there is a lot of misinformation about supplements on the internet, so you can easily be mislead with false information. If you do want to look into supplements yourself, I suggest taking a look at the Resources page for reliable online resources.</p>
+        <h1>Help Page</h1>
+          <div class="row">
+            <div class="col-4 mx-auto">
+              <div class="card border-0">
+                <div class="card-body">
+                  <h2>FAQs</h2>
+                  <h4>What does 'contraindications' mean?</h4>
+                  <p>According to Wikipedia, a contraindication is a condition or factor that serves as a reason to withhold a certain medical treatment due to the harm that it would cause the patient. This means that if you have a certain medical condition that would conflict with the use of a supplement, you should either avoid taking that supplement, or consult with your physician first before taking it.</p>
+                  <h4>What is a 'malady'?</h4>
+                  <p>A malady is defined as a disease or ailment.</p>
+                  <h4>My doctor doesn't know much about supplements. How do I find out which ones are right for me?</h4>
+                  <p>It is recommended to find a doctor who <em>is</em> educated on the use of supplements. These doctors are typically Chiropractors and Naturopathic Doctors. You may also want to consult an experienced herbalist or holistic nutritionist. It is best to avoid Googling because there is a lot of misinformation about supplements on the internet, so you can easily be mislead with false information. If you do want to look into supplements yourself, I suggest taking a look at the Resources page for reliable online resources.</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </>
     );
 }
