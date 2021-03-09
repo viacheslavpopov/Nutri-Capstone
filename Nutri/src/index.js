1d8a0b99a0246a99adb346d3e0f6f77c551bedbb
ilromali <iliashenkoroman@gmail.com>
2021-03-09 10:24:43 -0800

    import bootstrap

--- Nutri/src/index.js
+++ Nutri/src/index.js
@@ -8,6 +8,7 @@ import rootReducer from './reducers/index';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import middlewareLogger from './middleware/middleware-logger';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, middlewareLogger));
 
