5a51e974897fb4e4e2e1069681fc4bf74deec069
ilromali <iliashenkoroman@gmail.com>
2021-03-10 12:08:01 -0800

    change project info in config file

--- Nutri/src/firebase.js
+++ Nutri/src/firebase.js
@@ -1,15 +1,14 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-}
+  apiKey: "AIzaSyCkDG9Dm7lAqyIyPB_FPkFGdMHPrsIgj30",
+  authDomain: "nutri-5162d.firebaseapp.com",
+  projectId: "nutri-5162d",
+  storageBucket: "nutri-5162d.appspot.com",
+  messagingSenderId: "571066615904",
+  appId: "1:571066615904:web:50033323bb79acc99ddb05"
+};
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore();
