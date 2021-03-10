cc9b00efd49206c0ef764005d606f4500f5693db
ilromali <iliashenkoroman@gmail.com>
2021-03-09 16:03:57 -0800

    add second argument for updated method

--- Nutri/src/components/MaladyList.js
+++ Nutri/src/components/MaladyList.js
@@ -13,13 +13,13 @@ function MaladyList(props) {
               <h4>Welcome to the Nutri supplement database!</h4>
               <h5>Click the links below to view supplement suggestions.</h5>
               <hr />
-              <h2><button className="btn btn-info" onClick={ () => onSelectMalady('sugar') }>Blood Sugar Support</button></h2>
-              <h2><button className="btn btn-info" onClick={ () => onSelectMalady('detox') }>Detox Support</button></h2>
-              <h2><button className="btn btn-info" onClick={ () => onSelectMalady('immune') }>Immune System Support</button></h2>
-              <h2><button className="btn btn-info" onClick={ () => onSelectMalady('liver') }>Liver Health</button></h2>
-              <h2><button className="btn btn-info" onClick={ () => onSelectMalady('skin') }>Skin Health</button></h2>
-              <h2><button className="btn btn-info" onClick={ () => onSelectMalady('digestive') }>Digestive Health</button></h2>
-              <h2><button className="btn btn-info" onClick={ () => onSelectMalady('athletic') }>Athletic Performance</button></h2>
+              <h2><button className="btn btn-info" onClick={ () => onSelectMalady('sugar', 'Blood Sugar Support') }>Blood Sugar Support</button></h2>
+              <h2><button className="btn btn-info" onClick={ () => onSelectMalady('detox', 'Detox Support') }>Detox Support</button></h2>
+              <h2><button className="btn btn-info" onClick={ () => onSelectMalady('immune', 'Immune System Support') }>Immune System Support</button></h2>
+              <h2><button className="btn btn-info" onClick={ () => onSelectMalady('liver', 'Liver Health') }>Liver Health</button></h2>
+              <h2><button className="btn btn-info" onClick={ () => onSelectMalady('skin', 'Skin Health') }>Skin Health</button></h2>
+              <h2><button className="btn btn-info" onClick={ () => onSelectMalady('digestive', 'Digestive Health') }>Digestive Health</button></h2>
+              <h2><button className="btn btn-info" onClick={ () => onSelectMalady('athletic', 'Athletic Performance') }>Athletic Performance</button></h2>
             </div>
           </div>
         </div>
@@ -42,3 +42,6 @@ export default MaladyList;
 // should these both be in one function? 
 // error could be displayed on main page as before if an API call fails using error prop and isLoading
 // can I have a function doing 3 things from here? maybe.. dispatching 2+ actions with API call.
+
+
+// create another state in store, to hold malady or null, pull it using useSelector, and display it above the .map
