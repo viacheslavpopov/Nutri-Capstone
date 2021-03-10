33798300f8644b81b7c36eac48ac5baa7df1dcc2
ilromali <iliashenkoroman@gmail.com>
2021-03-09 16:55:09 -0800

    add migration to fix typos

---
+++ NutriAPI/Migrations/20210310002815_CorrectBeet.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NutriAPI.Migrations
+{
+    public partial class CorrectBeet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Supplements",
+                keyColumn: "SupplementId",
+                keyValue: 9,
+                column: "Dosing",
+                value: "No established dose. Use as directed on the supplement bottle.");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Supplements",
+                keyColumn: "SupplementId",
+                keyValue: 9,
+                column: "Dosing",
+                value: "No established dose. Use as directed on the supplement bottle. It's important to note that the body produces HMB naturally, but additional supplementation may benefit body-building efforts.");
+        }
+    }
+}
