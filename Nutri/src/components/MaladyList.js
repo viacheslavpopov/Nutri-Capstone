1bfdc028241786674d5c68b76313aa813fa121f8
ilromali <iliashenkoroman@gmail.com>
2021-03-10 16:42:52 -0800

    remove comments

--- Nutri/src/components/MaladyList.js
+++ Nutri/src/components/MaladyList.js
@@ -7,7 +7,7 @@ function MaladyList(props) {
   return (
     <>
       <div className="row">
-        <div className="col-6 mx-auto">
+        <div className="col-4 mx-auto">
           <div className="card border-0">
             <div className="card-body">
               <h4>Welcome to the Nutri supplement database!</h4>
@@ -33,15 +33,3 @@ MaladyList.propTypes = {
 }
 
 export default MaladyList;
-
-// click a malady.. an API call is made with that malady as a parameter?
-// could put the API call function in the malady list component
-
-//onSelectMalady toggles what is visible on page, and is triggered if a malady is clicked
-// after a malady is clicked, an API call needs to be made with that malady as the argument
-// should these both be in one function? 
-// error could be displayed on main page as before if an API call fails using error prop and isLoading
-// can I have a function doing 3 things from here? maybe.. dispatching 2+ actions with API call.
-
-
-// create another state in store, to hold malady or null, pull it using useSelector, and display it above the .map
