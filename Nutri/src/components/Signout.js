e6550e5f58022c51c7fd3804c089822e09fb7974
ilromali <iliashenkoroman@gmail.com>
2021-03-10 16:39:29 -0800

    add signout component

---
+++ Nutri/src/components/Signout.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import firebase from 'firebase/app';
+import * as a from './../actions';
+
+function Signout() {
+
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  function doSignOut() {
+    firebase.auth().signOut().then(function() {
+      console.log('Successfully signed out!');
+    }).catch(function(error) {
+      console.log(error.message);
+    });
+    dispatch(a.toggleLogin());
+    history.push('/');
+  }
+
+  function navigateToHome() {
+    history.push('/');
+  }
+
+  // function navigateToSignin() {
+  //   history.push('/signin');
+  // }
+
+
+  return (
+    <>
+      <button className='btn btn-info' onClick={doSignOut}>Sign Out</button>
+      <button className='btn btn-info' onClick={navigateToHome}>Back to Home</button>
+    </>
+  );
+}
+
+export default Signout;
